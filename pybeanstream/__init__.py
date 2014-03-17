c197ff2fe42d1d3b63a1bdb72b75f5264708584f
Benoit C. Sirois <benoit@clennett.com>
2014-03-17 16:40:33 -0400

    Fix for namespace

--- pybeanstream/__init__.py
+++ pybeanstream/__init__.py
@@ -17,3 +17,7 @@
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
 # MA 02110-1301  USA
+
+# Namespace declaration:
+__import__('pkg_resources').declare_namespace(__name__)
+
