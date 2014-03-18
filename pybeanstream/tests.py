bb98cc03d3bdea69d958a69fb27697d12269d904
Benoit C. Sirois <benoitcsirois@gmail.com>
2014-03-18 00:22:47 -0400

    Added to README, fixed order_num in tests

--- pybeanstream/tests.py
+++ pybeanstream/tests.py
@@ -25,7 +25,6 @@ from pybeanstream.client import (
 )
 from pybeanstream.xml_utils import xmltodict
 import unittest
-import random
 from mock import Mock
 
 
@@ -74,10 +73,8 @@ class TestApiTransactions(unittest.TestCase):
         self.b = BeanClient('a_username', 'a_password', 'a_merchant_id')
         self.b.suds_client = Mock()
         
-    def make_list(self, cc_num, cvv, exp_m, exp_y, amount='10.00', order_num=None):
+    def make_list(self, cc_num, cvv, exp_m, exp_y, amount, order_num):
         # Returns a prepared list with test data already filled in.
-        if not order_num:
-            order_num = str(random.randint(1000, 1000000))
         d = ('Jérémy Noël',
              cc_num,
              cvv,
@@ -103,7 +100,7 @@ class TestApiTransactions(unittest.TestCase):
 
         # Preparing data
         amount = '0.01'
-        order_num = str(random.randint(1000, 1000000))
+        order_num = '900581'
         cc_num = '4030000010001234'
         cvv = '123'
         exp_month = '05'
@@ -121,6 +118,7 @@ class TestApiTransactions(unittest.TestCase):
                             amount=amount,
                             order_num=order_num))
         self.assertTrue(result.data['trnApproved'])
+        self.assertEqual(result.data['trnOrderNumber'], order_num)
 
         # Executing pre-auth complete
         adj_id = result.data['trnId']
@@ -135,6 +133,7 @@ class TestApiTransactions(unittest.TestCase):
                     order_num,
                     adj_id)
         self.assertTrue(result.data['trnApproved'])
+        self.assertEqual(result.data['trnOrderNumber'], order_num)
 
     def test_pre_auth_bytes(self):
         """ Tests that a parameters passed to preauth_request method
@@ -142,7 +141,7 @@ class TestApiTransactions(unittest.TestCase):
 
         # Preparing data
         amt = '0.01'.encode('ascii')
-        order_num = str(random.randint(1000, 1000000)).encode('ascii')
+        order_num = '714409'
         dat = (
             b'Jeremy Noel',
             b'4030000010001234',
@@ -166,6 +165,7 @@ class TestApiTransactions(unittest.TestCase):
 
         result = self.b.preauth_request(*dat)
         self.assertTrue(result.data['trnApproved'])
+        self.assertEqual(result.data['trnOrderNumber'], order_num)
 
         # Executing pre-auth complete
         adj_id = result.data['trnId']
@@ -178,6 +178,7 @@ class TestApiTransactions(unittest.TestCase):
                     order_num,
                     adj_id)
         self.assertTrue(result.data['trnApproved'])
+        self.assertEqual(result.data['trnOrderNumber'], order_num)
 
     def test_unintelligible_error(self):
         """ This tests when the API returns an unexpected data
@@ -194,9 +195,14 @@ class TestApiTransactions(unittest.TestCase):
         self.b.suds_client.service.TransactionProcess.return_value = (
             '<response><trnApproved>1</trnApproved><trnId>10000679</trnId><messageId>1</messageId><messageText>Approved</messageText><trnOrderNumber>138889</trnOrderNumber><authCode>TEST</authCode><errorType>N</errorType><errorFields></errorFields><responseType>T</responseType><trnAmount>10.00</trnAmount><trnDate>3/17/2014 6:37:55 PM</trnDate><avsProcessed>1</avsProcessed><avsId>N</avsId><avsResult>0</avsResult><avsAddrMatch>0</avsAddrMatch><avsPostalMatch>0</avsPostalMatch><avsMessage>Street address and Postal/ZIP do not match.</avsMessage><cvdId>1</cvdId><cardType>VI</cardType><trnType>P</trnType><paymentMethod>CC</paymentMethod><ref1></ref1><ref2></ref2><ref3></ref3><ref4></ref4><ref5></ref5></response>')
 
+        order_num = '138889'
         result = self.b.purchase_request(
-            *self.make_list('4030000010001234', '123', '05', '15'))
+            *self.make_list(
+                '4030000010001234', '123', '05', '15',
+                '10.00', order_num))
         self.assertTrue(result.data['trnApproved'])
+        self.assertEqual(result.data['trnOrderNumber'], order_num)
+
 
     def test_purchase_transaction_visa_approve_2_address_lines(self):
         """ This tests a standard Purchase transaction with VISA and verifies
@@ -205,10 +211,15 @@ class TestApiTransactions(unittest.TestCase):
         self.b.suds_client.service.TransactionProcess.return_value = (
             '<response><trnApproved>1</trnApproved><trnId>10000680</trnId><messageId>1</messageId><messageText>Approved</messageText><trnOrderNumber>596771</trnOrderNumber><authCode>TEST</authCode><errorType>N</errorType><errorFields></errorFields><responseType>T</responseType><trnAmount>10.00</trnAmount><trnDate>3/17/2014 6:37:56 PM</trnDate><avsProcessed>1</avsProcessed><avsId>N</avsId><avsResult>0</avsResult><avsAddrMatch>0</avsAddrMatch><avsPostalMatch>0</avsPostalMatch><avsMessage>Street address and Postal/ZIP do not match.</avsMessage><cvdId>1</cvdId><cardType>VI</cardType><trnType>P</trnType><paymentMethod>CC</paymentMethod><ref1></ref1><ref2></ref2><ref3></ref3><ref4></ref4><ref5></ref5></response>')
 
+        order_num = '596771'
         result = self.b.purchase_request(
-            *self.make_list('4030000010001234', '123', '05', '15'),
+            *self.make_list(
+                '4030000010001234', '123', '05', '15',
+                '10.00', order_num),
              **{'cust_address_line2': 'rr2'})
         self.assertTrue(result.data['trnApproved'])
+        self.assertTrue(result.data['trnOrderNumber'], order_num)
+
 
     def test_purchase_transaction_visa_declined(self):
         """ This tests a failing Purchase transaction with VISA and verifies
@@ -217,10 +228,15 @@ class TestApiTransactions(unittest.TestCase):
         self.b.suds_client.service.TransactionProcess.return_value = (
             '<response><trnApproved>0</trnApproved><trnId>10000681</trnId><messageId>7</messageId><messageText>DECLINE</messageText><trnOrderNumber>554213</trnOrderNumber><authCode></authCode><errorType>N</errorType><errorFields></errorFields><responseType>T</responseType><trnAmount>10.00</trnAmount><trnDate>3/17/2014 6:37:57 PM</trnDate><avsProcessed>1</avsProcessed><avsId>N</avsId><avsResult>0</avsResult><avsAddrMatch>0</avsAddrMatch><avsPostalMatch>0</avsPostalMatch><avsMessage>Street address and Postal/ZIP do not match.</avsMessage><cvdId>1</cvdId><cardType>VI</cardType><trnType>P</trnType><paymentMethod>CC</paymentMethod><ref1></ref1><ref2></ref2><ref3></ref3><ref4></ref4><ref5></ref5></response>')
 
+        order_num = '554213'
         result = self.b.purchase_request(
-            *self.make_list('4003050500040005', '123', '05', '15'))
+            *self.make_list(
+                '4003050500040005', '123', '05', '15',
+                '10.00', order_num))
         self.assertFalse(result.data['trnApproved'])
+        self.assertEqual(result.data['trnOrderNumber'], order_num)
                 
+
     def test_purchase_transaction_visa_declined_cvd_ok(self):
         """ This tests a failing Purchase transaction with VISA and verifies
         that the correct response is returned, this is declined for
@@ -228,9 +244,14 @@ class TestApiTransactions(unittest.TestCase):
         self.b.suds_client.service.TransactionProcess.return_value = (
             '<response><trnApproved>0</trnApproved><trnId>10000682</trnId><messageId>7</messageId><messageText>DECLINE</messageText><trnOrderNumber>722130</trnOrderNumber><authCode></authCode><errorType>N</errorType><errorFields></errorFields><responseType>T</responseType><trnAmount>101.00</trnAmount><trnDate>3/17/2014 6:37:58 PM</trnDate><avsProcessed>1</avsProcessed><avsId>N</avsId><avsResult>0</avsResult><avsAddrMatch>0</avsAddrMatch><avsPostalMatch>0</avsPostalMatch><avsMessage>Street address and Postal/ZIP do not match.</avsMessage><cvdId>1</cvdId><cardType>VI</cardType><trnType>P</trnType><paymentMethod>CC</paymentMethod><ref1></ref1><ref2></ref2><ref3></ref3><ref4></ref4><ref5></ref5></response>')
 
+        order_num = '722130'
         result = self.b.purchase_request(
-            *self.make_list('4504481742333', '123', '05', '15', amount='101.00'))
+            *self.make_list(
+                '4504481742333', '123', '05', '15',
+                '101.00', order_num))
         self.assertFalse(result.data['trnApproved'])
+        self.assertEqual(result.data['trnOrderNumber'], order_num)
+
 
     def test_purchase_transaction_amex_approve(self):
         """ This tests a standard Purchase transaction with AMEX and verifies
@@ -239,9 +260,14 @@ class TestApiTransactions(unittest.TestCase):
         self.b.suds_client.service.TransactionProcess.return_value = (
             '<response><trnApproved>1</trnApproved><trnId>10000675</trnId><messageId>1</messageId><messageText>Approved</messageText><trnOrderNumber>458165</trnOrderNumber><authCode>TEST</authCode><errorType>N</errorType><errorFields></errorFields><responseType>T</responseType><trnAmount>10.00</trnAmount><trnDate>3/17/2014 6:37:53 PM</trnDate><avsProcessed>1</avsProcessed><avsId>N</avsId><avsResult>0</avsResult><avsAddrMatch>0</avsAddrMatch><avsPostalMatch>0</avsPostalMatch><avsMessage>Street address and Postal/ZIP do not match.</avsMessage><cvdId>1</cvdId><cardType>AM</cardType><trnType>P</trnType><paymentMethod>CC</paymentMethod><ref1></ref1><ref2></ref2><ref3></ref3><ref4></ref4><ref5></ref5></response>')
 
+        order_num = '458165'
         result = self.b.purchase_request(
-            *self.make_list('371100001000131', '1234', '05', '15'))
+            *self.make_list(
+                '371100001000131', '1234', '05', '15',
+                '10.00', order_num))
         self.assertTrue(result.data['trnApproved'])
+        self.assertEqual(result.data['trnOrderNumber'], order_num)
+
 
     def test_purchase_transaction_amex_declined(self):
         """ This tests a failing Purchase transaction with AMEX and verifies
@@ -250,9 +276,14 @@ class TestApiTransactions(unittest.TestCase):
         self.b.suds_client.service.TransactionProcess.return_value = (
             '<response><trnApproved>0</trnApproved><trnId>10000676</trnId><messageId>7</messageId><messageText>DECLINE</messageText><trnOrderNumber>189112</trnOrderNumber><authCode></authCode><errorType>N</errorType><errorFields></errorFields><responseType>T</responseType><trnAmount>10.00</trnAmount><trnDate>3/17/2014 6:37:53 PM</trnDate><avsProcessed>1</avsProcessed><avsId>N</avsId><avsResult>0</avsResult><avsAddrMatch>0</avsAddrMatch><avsPostalMatch>0</avsPostalMatch><avsMessage>Street address and Postal/ZIP do not match.</avsMessage><cvdId>1</cvdId><cardType>AM</cardType><trnType>P</trnType><paymentMethod>CC</paymentMethod><ref1></ref1><ref2></ref2><ref3></ref3><ref4></ref4><ref5></ref5></response>')
 
+        order_num = '189112'
         result = self.b.purchase_request(
-            *self.make_list('342400001000180', '1234', '05', '15'))
+            *self.make_list(
+                '342400001000180', '1234', '05', '15',
+                '10.00', order_num))
         self.assertFalse(result.data['trnApproved'])
+        self.assertEqual(result.data['trnOrderNumber'], order_num)
+
 
     def test_purchase_transaction_mastercard_approve(self):
         """ This tests a standard Purchase transaction with mastercard and verifies
@@ -261,9 +292,14 @@ class TestApiTransactions(unittest.TestCase):
         self.b.suds_client.service.TransactionProcess.return_value = (
             '<response><trnApproved>1</trnApproved><trnId>10000677</trnId><messageId>1</messageId><messageText>Approved</messageText><trnOrderNumber>590048</trnOrderNumber><authCode>TEST</authCode><errorType>N</errorType><errorFields></errorFields><responseType>T</responseType><trnAmount>10.00</trnAmount><trnDate>3/17/2014 6:37:54 PM</trnDate><avsProcessed>1</avsProcessed><avsId>N</avsId><avsResult>0</avsResult><avsAddrMatch>0</avsAddrMatch><avsPostalMatch>0</avsPostalMatch><avsMessage>Street address and Postal/ZIP do not match.</avsMessage><cvdId>1</cvdId><cardType>MC</cardType><trnType>P</trnType><paymentMethod>CC</paymentMethod><ref1></ref1><ref2></ref2><ref3></ref3><ref4></ref4><ref5></ref5></response>')
 
+        order_num = '590048'
         result = self.b.purchase_request(
-            *self.make_list('5100000010001004', '123', '05', '15'))
+            *self.make_list(
+                '5100000010001004', '123', '05', '15',
+                '10.00', order_num))
         self.assertTrue(result.data['trnApproved'])
+        self.assertEqual(result.data['trnOrderNumber'], order_num)
+
 
     def test_purchase_transaction_mastercard_declined(self):
         """ This tests a failing Purchase transaction with mastercard and verifies
@@ -272,9 +308,13 @@ class TestApiTransactions(unittest.TestCase):
         self.b.suds_client.service.TransactionProcess.return_value = (
             '<response><trnApproved>0</trnApproved><trnId>10000678</trnId><messageId>7</messageId><messageText>DECLINE</messageText><trnOrderNumber>446429</trnOrderNumber><authCode></authCode><errorType>N</errorType><errorFields></errorFields><responseType>T</responseType><trnAmount>10.00</trnAmount><trnDate>3/17/2014 6:37:55 PM</trnDate><avsProcessed>1</avsProcessed><avsId>N</avsId><avsResult>0</avsResult><avsAddrMatch>0</avsAddrMatch><avsPostalMatch>0</avsPostalMatch><avsMessage>Street address and Postal/ZIP do not match.</avsMessage><cvdId>1</cvdId><cardType>MC</cardType><trnType>P</trnType><paymentMethod>CC</paymentMethod><ref1></ref1><ref2></ref2><ref3></ref3><ref4></ref4><ref5></ref5></response>')
 
+        order_num = '446429'
         result = self.b.purchase_request(
-            *self.make_list('5100000020002000', '123', '05', '15'))
+            *self.make_list(
+                '5100000020002000', '123', '05', '15',
+                '10.00', order_num))
         self.assertFalse(result.data['trnApproved'])
+        self.assertEqual(result.data['trnOrderNumber'], order_num)
 
 
     def test_refund(self):
@@ -284,12 +324,14 @@ class TestApiTransactions(unittest.TestCase):
         self.b.suds_client.service.TransactionProcess.return_value = (
             '<response><trnApproved>1</trnApproved><trnId>10000800</trnId><messageId>1</messageId><messageText>Approved</messageText><trnOrderNumber>567121</trnOrderNumber><authCode>TEST</authCode><errorType>N</errorType><errorFields></errorFields><responseType>T</responseType><trnAmount>0.01</trnAmount><trnDate>3/17/2014 8:37:04 PM</trnDate><avsProcessed>1</avsProcessed><avsId>N</avsId><avsResult>0</avsResult><avsAddrMatch>0</avsAddrMatch><avsPostalMatch>0</avsPostalMatch><avsMessage>Street address and Postal/ZIP do not match.</avsMessage><cvdId>2</cvdId><cardType>AM</cardType><trnType>R</trnType><paymentMethod>CC</paymentMethod><ref1></ref1><ref2></ref2><ref3></ref3><ref4></ref4><ref5></ref5></response>')
 
+        order_num = '567121'
         result = self.b.refund_request(
             amount='0.01',
-            order_num='567121',
+            order_num=order_num,
             adj_id='10000787')
 
         self.assertTrue(result.data['trnApproved'])
+        self.assertEqual(result.data['trnOrderNumber'], order_num)
 
 
     def test_voids(self):
@@ -299,9 +341,11 @@ class TestApiTransactions(unittest.TestCase):
         self.b.suds_client.service.TransactionProcess.return_value = (
             '<response><trnApproved>1</trnApproved><trnId>10000804</trnId><messageId>1</messageId><messageText>Approved</messageText><trnOrderNumber>243364</trnOrderNumber><authCode>TEST</authCode><errorType>N</errorType><errorFields></errorFields><responseType>T</responseType><trnAmount>10.00</trnAmount><trnDate>3/17/2014 8:42:24 PM</trnDate><avsProcessed>1</avsProcessed><avsId>N</avsId><avsResult>0</avsResult><avsAddrMatch>0</avsAddrMatch><avsPostalMatch>0</avsPostalMatch><avsMessage>Street address and Postal/ZIP do not match.</avsMessage><cvdId>2</cvdId><cardType>VI</cardType><trnType>VP</trnType><paymentMethod>CC</paymentMethod><ref1></ref1><ref2></ref2><ref3></ref3><ref4></ref4><ref5></ref5></response>')
 
+        order_num = '243364'
         result = self.b.void_request(
             amount='10.00',
-            order_num='243364',
+            order_num=order_num,
             adj_id='10000770')
 
         self.assertTrue(result.data['trnApproved'])
+        self.assertEqual(result.data['trnOrderNumber'], order_num)
